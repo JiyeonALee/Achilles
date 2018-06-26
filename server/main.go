af909cc02d0948083fafc793408fb0b189df87b8
zer0ize <zer0ize@nate.com>
2018-06-27 03:05:31 +0900

    getScore() Json 파싱하여 리턴하도록 수정

--- server/main.go
+++ server/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/backendservice/Achilles/achilles"
@@ -57,11 +62,423 @@ func (s *server) Compute(context.Context, *achilles.AchillesRequest) (*achilles.
 	result, err := SortScores(x)
 	return result, err
 }
-func getScore() []nationScore {
-	return scores
+
+func getScore(nationTxt string) (nation_group string, nation string, nation_scores []int) {
+	raw, err := ioutil.ReadFile("./score.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var data WorldCup_Data
+	json.Unmarshal(raw, &data)
+
+	nation_id, nation := getNationData(data, nationTxt)
+	isValid := false
+
+	nation_group, nation_scores, isValid = getScoreFromGroupA(data, nation_id)
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupA(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupB(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupC(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupD(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupE(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupF(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupG(data, nation_id)
+	}
+
+	if !isValid {
+		nation_group, nation_scores, isValid = getScoreFromGroupH(data, nation_id)
+	}
+
+	return
+}
+
+func getNationData(data WorldCup_Data, nationTxt string) (nation_id int, nation string) {
+
+	for _, team := range data.Teams {
+		if strings.Contains(strings.ToLower(team.Name), strings.ToLower(nationTxt)) {
+			nation_id = team.ID
+			nation = team.Name
+			break
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupA(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.A.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "A"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.A.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupB(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.B.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "B"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.B.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupC(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.C.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "C"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.C.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupD(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.D.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "D"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.D.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupE(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.E.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "E"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.E.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupF(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.F.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "F"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.F.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupG(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.G.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "G"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.G.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func getScoreFromGroupH(data WorldCup_Data, nation_id int) (nation_group string, nation_scores []int, isValid bool) {
+
+	for _, match := range data.Groups.H.Matches {
+		if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+			nation_group = "H"
+			break
+		}
+	}
+
+	isValid = nation_group != ""
+	if !isValid {
+		return
+	}
+
+	if nation_group != "" {
+		for _, match := range data.Groups.H.Matches {
+			if match.Finished {
+				if match.HomeTeam == nation_id || match.AwayTeam == nation_id {
+					isHomeTeam := match.HomeTeam == nation_id
+
+					if match.HomeResult > match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 3)
+						} else {
+							nation_scores = append(nation_scores, 0)
+						}
+					} else if match.HomeResult < match.AwayResult {
+						if isHomeTeam {
+							nation_scores = append(nation_scores, 0)
+						} else {
+							nation_scores = append(nation_scores, 3)
+						}
+					} else {
+						nation_scores = append(nation_scores, 1)
+					}
+				}
+			}
+		}
+	}
+
+	return
 }
 
 func main() {
+
+	nation_group, nation, nation_scores := getScore("korea")
+
+	fmt.Println(nation_group)
+	fmt.Println(nation)
+	for _, score := range nation_scores {
+		fmt.Println(score)
+	}
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -74,5 +491,4 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
 }
--- server/score.go
+++ server/score.go
@@ -1,28 +1,301 @@
 package main
 
-type nationScore struct {
-	Team string
-	Nation string
-	Score []int32
-}
+import "time"
 
-var scores = []nationScore{
-	{
-		"F",
-		"KOR",
-		[]int32{0,0},
-	},
-	{
-		"F",
-		"MEX",
-		[]int32{3,3},
-	},	{
-		"F",
-		"SWE",
-		[]int32{3,0},
-	},	{
-		"F",
-		"GER",
-		[]int32{0,3},
-	},
+// type nationScore struct {
+// 	Group  string
+// 	Nation string
+// 	Score  []int32
+// }
+
+// var scores = []nationScore{
+// 	{
+// 		"F",
+// 		"KOR",
+// 		[]int32{0, 0},
+// 	},
+// 	{
+// 		"F",
+// 		"MEX",
+// 		[]int32{3, 3},
+// 	}, {
+// 		"F",
+// 		"SWE",
+// 		[]int32{3, 0},
+// 	}, {
+// 		"F",
+// 		"GER",
+// 		[]int32{0, 3},
+// 	},
+// }
+
+type WorldCup_Data struct {
+	Stadiums []struct {
+		ID    int     `json:"id"`
+		Name  string  `json:"name"`
+		City  string  `json:"city"`
+		Lat   float64 `json:"lat"`
+		Lng   float64 `json:"lng"`
+		Image string  `json:"image"`
+	} `json:"stadiums"`
+	Tvchannels []struct {
+		ID      int      `json:"id"`
+		Name    string   `json:"name"`
+		Icon    string   `json:"icon"`
+		Country string   `json:"country"`
+		Iso2    string   `json:"iso2"`
+		Lang    []string `json:"lang"`
+	} `json:"tvchannels"`
+	Teams []struct {
+		ID          int    `json:"id"`
+		Name        string `json:"name"`
+		FifaCode    string `json:"fifaCode"`
+		Iso2        string `json:"iso2"`
+		Flag        string `json:"flag"`
+		Emoji       string `json:"emoji"`
+		EmojiString string `json:"emojiString"`
+	} `json:"teams"`
+	Groups struct {
+		A struct {
+			Name     string `json:"name"`
+			Winner   int    `json:"winner"`
+			Runnerup int    `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"a"`
+		B struct {
+			Name     string `json:"name"`
+			Winner   int    `json:"winner"`
+			Runnerup int    `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"b"`
+		C struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"c"`
+		D struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"d"`
+		E struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"e"`
+		F struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"f"`
+		G struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"g"`
+		H struct {
+			Name     string      `json:"name"`
+			Winner   interface{} `json:"winner"`
+			Runnerup interface{} `json:"runnerup"`
+			Matches  []struct {
+				Name       int       `json:"name"`
+				Type       string    `json:"type"`
+				HomeTeam   int       `json:"home_team"`
+				AwayTeam   int       `json:"away_team"`
+				HomeResult int       `json:"home_result"`
+				AwayResult int       `json:"away_result"`
+				Date       time.Time `json:"date"`
+				Stadium    int       `json:"stadium"`
+				Channels   []int     `json:"channels"`
+				Finished   bool      `json:"finished"`
+				Matchday   int       `json:"matchday"`
+			} `json:"matches"`
+		} `json:"h"`
+	} `json:"groups"`
+	Knockout struct {
+		Round16 struct {
+			Name    string `json:"name"`
+			Matches []struct {
+				Name        int         `json:"name"`
+				Type        string      `json:"type"`
+				HomeTeam    int         `json:"home_team"`
+				AwayTeam    int         `json:"away_team"`
+				HomeResult  interface{} `json:"home_result"`
+				AwayResult  interface{} `json:"away_result"`
+				HomePenalty interface{} `json:"home_penalty"`
+				AwayPenalty interface{} `json:"away_penalty"`
+				Winner      interface{} `json:"winner"`
+				Date        time.Time   `json:"date"`
+				Stadium     int         `json:"stadium"`
+				Channels    []int       `json:"channels"`
+				Finished    bool        `json:"finished"`
+				Matchday    int         `json:"matchday"`
+			} `json:"matches"`
+		} `json:"round_16"`
+		Round8 struct {
+			Name    string `json:"name"`
+			Matches []struct {
+				Name        int         `json:"name"`
+				Type        string      `json:"type"`
+				HomeTeam    int         `json:"home_team"`
+				AwayTeam    int         `json:"away_team"`
+				HomeResult  interface{} `json:"home_result"`
+				AwayResult  interface{} `json:"away_result"`
+				HomePenalty interface{} `json:"home_penalty"`
+				AwayPenalty interface{} `json:"away_penalty"`
+				Winner      interface{} `json:"winner"`
+				Date        time.Time   `json:"date"`
+				Stadium     int         `json:"stadium"`
+				Channels    []int       `json:"channels"`
+				Finished    bool        `json:"finished"`
+				Matchday    int         `json:"matchday"`
+			} `json:"matches"`
+		} `json:"round_8"`
+		Round4 struct {
+			Name    string `json:"name"`
+			Matches []struct {
+				Name        int         `json:"name"`
+				Type        string      `json:"type"`
+				HomeTeam    int         `json:"home_team"`
+				AwayTeam    int         `json:"away_team"`
+				HomeResult  interface{} `json:"home_result"`
+				AwayResult  interface{} `json:"away_result"`
+				HomePenalty interface{} `json:"home_penalty"`
+				AwayPenalty interface{} `json:"away_penalty"`
+				Winner      interface{} `json:"winner"`
+				Date        time.Time   `json:"date"`
+				Stadium     int         `json:"stadium"`
+				Channels    []int       `json:"channels"`
+				Finished    bool        `json:"finished"`
+				Matchday    int         `json:"matchday"`
+			} `json:"matches"`
+		} `json:"round_4"`
+		Round2Loser struct {
+			Name    string `json:"name"`
+			Matches []struct {
+				Name        int         `json:"name"`
+				Type        string      `json:"type"`
+				HomeTeam    int         `json:"home_team"`
+				AwayTeam    int         `json:"away_team"`
+				HomeResult  interface{} `json:"home_result"`
+				AwayResult  interface{} `json:"away_result"`
+				HomePenalty interface{} `json:"home_penalty"`
+				AwayPenalty interface{} `json:"away_penalty"`
+				Winner      interface{} `json:"winner"`
+				Date        time.Time   `json:"date"`
+				Stadium     int         `json:"stadium"`
+				Channels    []int       `json:"channels"`
+				Finished    bool        `json:"finished"`
+				Matchday    int         `json:"matchday"`
+			} `json:"matches"`
+		} `json:"round_2_loser"`
+		Round2 struct {
+			Name    string `json:"name"`
+			Matches []struct {
+				Name        int         `json:"name"`
+				Type        string      `json:"type"`
+				HomeTeam    int         `json:"home_team"`
+				AwayTeam    int         `json:"away_team"`
+				HomeResult  interface{} `json:"home_result"`
+				AwayResult  interface{} `json:"away_result"`
+				HomePenalty interface{} `json:"home_penalty"`
+				AwayPenalty interface{} `json:"away_penalty"`
+				Winner      interface{} `json:"winner"`
+				Date        time.Time   `json:"date"`
+				Stadium     int         `json:"stadium"`
+				Channels    []int       `json:"channels"`
+				Finished    bool        `json:"finished"`
+				Matchday    int         `json:"matchday"`
+			} `json:"matches"`
+		} `json:"round_2"`
+	} `json:"knockout"`
 }
